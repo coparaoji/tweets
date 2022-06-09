1b19705dbf3b5674985f440f5f943d654559938b
Alex Oparaoji <coparaoji@gmail.com>
2022-06-09 13:40:15 -0400

    App running with tests

--- app.py
+++ app.py
@@ -1,78 +1,19 @@
-from contextlib import redirect_stderr
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////databases/tweets'
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+from flask import render_template
+import sys
+import os
+sys.path.append(os.path.dirname('..'))
+import factory
 
 
-class Tweet(db.Model):
-    __name__ = 'tweet'
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(241), nullable=False)
-    player_id = db.Column(db.String(50), unique=True, nullable=False)
-    sentiment_score = db.Column(db.Float, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
+app = factory.create_app()
 
-    def __repr__(self):
-        return (f"<Tweets id:{id} text:{text} created_at:{created_at}")
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def index():
-   if False:
+   from utilities import dbs
+   data = dbs.get_data()
+   return render_template('index.html', data=data)
 
-      #create a new task from form input
-      task_content =request.form['content']
-      new_task = Todo(content = task_content)
-      
-      #try to commit to database then redirect back to home
-      try:
-         db.session.add(new_task)
-         db.session.commit()
-         return redirect('/')
-      except:
-         return 'There was en issue adding your task'
-   else:
 
-      #look at created items in database and return 
-      #tasks = Todo.query.order_by(Todo.date_created).all()
-      return render_template('index.html')#, tasks=tasks)
-
-@app.route('/delete/<int:id>')
-def delete(id):
-   
-   #get task or give 404
-   task_to_delete = Todo.query.get_or_404(id)
-   
-   #attaempt to deletefrom database
-   try:
-      db.session.delete(task_to_delete)
-      db.session.commit()
-      return redirect('/')
-   except:
-      return 'There was an issue deleting that task'
-
-@app.route('/update/<int:id>',methods=['GET',"POST"])
-def update(id):
-   task = Todo.query.get_or_404(id)
-
-   if request.method == 'POST':
-      #update logic when button is pressed
-      task.content = request.form['content']
-
-      #try to commit to database
-      try:
-         db.session.commit()
-         return redirect('/')
-      except:
-         return 'There was an issue updating your task.'
-   else:
-      return render_template('update.html', task=task)
-  
 
 if __name__ == '__main__':
    app.run(debug=True)
---
+++ utilities/dbs.py
@@ -0,0 +1,83 @@
+import sqlite3
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+def create_team(conn, team):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param team:
+    :return: project id
+    """
+    sql = ''' INSERT INTO team(team_name,num_tweets,score)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, team)
+    conn.commit()
+    return cur.lastrowid
+
+    
+def create_team(conn, team):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param team:
+    :return: project id
+    """
+    sql = ''' INSERT INTO team(team_name,num_tweets,score)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, team)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_player(conn, player):
+    """
+    Create a new task
+    :param conn:
+    :param player:
+    :return:
+    """
+
+    sql = ''' INSERT INTO player(number,name,team_name,player_id)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, player)
+    conn.commit()
+    return cur.lastrowid
+
+def create_tweet(conn, tweet):
+    """
+    Create a new task
+    :param conn:
+    :param tweet:
+    :return:
+    """
+
+    sql = ''' INSERT INTO tweet(unique_id,tweet_id,player_id,text,score,magnitude,resultant,created_at)
+              VALUES(?,?,?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, tweet)
+    conn.commit()
+    return cur.lastrowid
+
+def get_data():
+   database = "/home/alex/GreyCroc/tweets/Databases/tweets.db"
+   conn =  create_connection(database)
+   sql_query = '''SELECT number,name,num_tweets,res_avg FROM player'''
+   cursor = conn.cursor()
+   cursor.execute(sql_query)
+   rows = cursor.fetchall()
+   return rows
